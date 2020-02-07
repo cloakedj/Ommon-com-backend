f9d6369b85d350fdc3bd06ba8f6074e07e57248f
unknown <kanwardhananjaysingh0545@gmail.com>
2020-02-07 11:25:01 +0530

    Updated

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 const uri="mongodb+srv://kanwardhananjay:1999101275D@cluster0-qinrs.mongodb.net/test?retryWrites=true&w=majority"
-mongoose.connect(uri,{useNewUrlParser: true,useUnifiedTopology:true})
+mongoose.connect(uri,{useNewUrlParser: true,useUnifiedTopology:true,dbName:"test"})
 .then(() => console.log("Connection To Mongoose Is Successful"))
 .catch((err) => console.log("Something Went Wrong",err))
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const port = 3000 || process.env.PORT;
 const morgan = require('morgan');
 const db = require('./config/dbConnection');
+const cors = require('cors');
 
 const userGateway = require("./routes/UserGateway");
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 app.use('/UserGateway',userGateway);
 app.listen(port);
