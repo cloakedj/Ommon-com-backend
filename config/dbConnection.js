79082b11235e60411fcbe4655d52daed0a8564af
Dhananjaykanwar <kanwardhananjaysingh0545@gmail.com>
2021-07-26 17:49:55 +0530

    Removed Connection string

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const uri="mongodb+srv://kanwardhananjay:1999101275D@cluster0-qinrs.mongodb.net/test?retryWrites=true&w=majority"
-mongoose.connect(uri,{useNewUrlParser: true,useUnifiedTopology:true,dbName:"test"})
+const uri=//Mongo URI
+mongoose.connect(uri,{useNewUrlParser: true,useUnifiedTopology:true,dbName://DBName
+})
 .then(() => console.log("Connection To Mongoose Is Successful"))
 .catch((err) => console.log("Something Went Wrong",err))
